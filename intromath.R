e370061789a7c60a4d1593f9943144eb43670eaf
ochienggogo <ochienggogo@gmail.com>
2019-03-27 17:27:56 +0300

    math operations

---
+++ intromath.R
@@ -0,0 +1,63 @@
+## Name: Steven Gogo
+## Date: 19th March 2019
+## Description: This script is being used to show how to handle basic arithmetics in R
+## I swear that this is easy. Enjoy!
+
+##1. Addition
+a<-220.2+678.1
+a
+b<-345.4+100.3
+b
+c<-673.5+782.5
+c
+
+#Subtraction 
+d<-130.4-109.6
+d
+e<-2746-898
+e
+f<-3546.78-783.5
+f
+i<-36373-3938
+i
+
+#Multiplication 
+j<-346*6762
+j
+k<-6733.39*893
+k
+l<-4645*37373
+l
+
+#Division 
+m<-462272/3455
+m
+n<-373678198.4/4772.56
+n
+p<-1000010/1000
+p
+
+#power
+q<-7288^678
+q
+r<-465.2^532
+r
+s<-562^100
+s
+
+#modulo 
+t<-1000%%100
+t
+u<-1938391%%345
+u
+v<-656%%656
+v
+
+#exponential 
+w<-exp(10)
+w
+x<-exp(1)
+x
+y<-exp(100)
+y
+
